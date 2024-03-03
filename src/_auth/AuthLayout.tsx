634504dbf5b92af7235a12f431f438bafa4dc9f8
Dhanush2468 <dhanushgollavilli02@gmail.com>
2024-03-03 05:54:59 +0530

    Final Version

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -16,7 +16,7 @@ export default function AuthLayout() {
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/side-img.jpg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
